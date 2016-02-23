a59dd094be12dc2bfb51a79d0fab1252266a0623
Laura White <laura.white+github@gmail.com>
2016-02-22 21:44:43 -0700

    answers for travis check, ec tbd

--- answers.sh
+++ answers.sh
@@ -43,7 +43,12 @@ echo "answer-2: $answer_2"
 # peak (i.e., interval) that has the largest mean signal in 
 # ctcf.hela.chr22.bg.gz.
 # MAP
-answer_3=
+answer_3=$(bedtools map -a \
+        <(gzcat $encode | awk '$4=="CTCF"') -b $bedgraph \
+        -c 4 -o mean \
+        | sort -k5 -nr \
+        | head -n1 \
+        | awk 'BEGIN{OFS="\t"} {print $3 -$2}')
 
 echo "answer-3: $answer_3"
 
@@ -51,7 +56,13 @@ echo "answer-3: $answer_3"
 # 1000 bp upstream of a TSS) w/ the highest median signal in
 # ctcf.hela.chr22.bg.gz. Report the gene name (e.g., 'ABC123')
 # FLANK
-answer_4=
+answer_4=$(bedtools flank -i $TSS -g $hg19gen -s -r 1000 -l 1000\
+        | bedtools sort -i - \
+        | bedtools map -a - -b $bedgraph -c 4 -o median \
+        | awk '$7 != "."' \
+        | sort -k7nr \
+        | head -n1 \
+        | cut -f4 )
 
 echo "answer-4: $answer_4"
 
@@ -59,7 +70,10 @@ echo "answer-4: $answer_4"
 # that is not covered by genes.hg19.bed. Report the interval
 # like chr1: 100-500.
 # COMPLEMENT
-answer_5=
+answer_5=$(bedtools complement -i $hg19bed -g $hg19gen \
+        | sort -k4nr \
+        | head -n1 \
+        | awk '{print $1":"$2"-"$3}')
 
 echo "answer-5: $answer_5"
 
